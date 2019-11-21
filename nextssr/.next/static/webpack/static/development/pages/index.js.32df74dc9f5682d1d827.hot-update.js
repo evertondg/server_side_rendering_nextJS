50d0266a0ad9f9b0637462bc0870831676b08506
Everton De Grande <evertondg@hotmail.com.br>
2019-11-21 13:25:36 -0300

    Commit Inicial

---
+++ nextssr/.next/static/webpack/static/development/pages/index.js.32df74dc9f5682d1d827.hot-update.js
@@ -0,0 +1,33 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "C:\\Users\\everton.degrande\\Documents\\GitHub\\server_side_rendering_nextJS\\nextssr\\pages\\index.js";
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var Home = function Home() {
+  return __jsx("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, "Hello World");
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Home);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.32df74dc9f5682d1d827.hot-update.js.map
---
+++ nextssr/pages/users.js
@@ -0,0 +1,33 @@
+import React from "react";
+import axios from "axios";
+
+import Link from "next/link";
+import Head from "next/head";
+
+const User = ({ users }) => (
+  <div>
+    <Head>
+      <title>Users</title>
+    </Head>
+    <ul>
+      {users.map(user => (
+        <li key={String(user.id)}>{user.login}</li>
+      ))}
+    </ul>
+    <Link href="/">
+      <a>Voltar</a>
+    </Link>
+  </div>
+);
+
+User.getInitialProps = async () => {
+  const response = await axios.get(
+    "https://api.github.com/orgs/rocketseat/members"
+  );
+
+  console.log(response.data);
+
+  return { users: response.data };
+};
+
+export default User;
